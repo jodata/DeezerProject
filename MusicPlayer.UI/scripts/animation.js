56050948cedfa742c4fc88a54cc4caf6003a55e3
Rémy <drouet.remy@gmail.com>
2015-04-13 16:56:01 +0200

    js pour le changement de style de l'onglet actif dans nav.html

--- MusicPlayer.UI/scripts/animation.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("a").on("click",function(){
-        alert("click");
-        $(".navbar-nav>li.active").removeClass("active");
-        $(this).addClass("active");
-    });
-});
