5e0c77d18cf419a85368816db52529851a0da6bc
Rémy <drouet.remy@gmail.com>
2015-04-20 20:55:16 +0200

    player navbar correction affichage gestion playlist vide

--- MusicPlayer.REST/MusicPlayerService.js
+++ MusicPlayer.REST/MusicPlayerService.js
@@ -16,6 +16,7 @@ var tracks;
 var currentTrack   = null;
 var afterTrack   = null;
 var proposedTracks = [];
+var position = 0;
 
 db.get('Albums', { data: true }, function(err, body) {
     if (!err){
@@ -28,7 +29,8 @@ db.get('Albums', { data: true }, function(err, body) {
                         for(var i=0;i<body.data.length;i++){
                             var prevTrack = body.data[i];
                             var track = tracks[prevTrack.trackID];
-                            proposedTracks.push({trackID:prevTrack.trackID,deezerID:track.deezerID,title:track.title,artist:albums[track.albumID].artist,cover:albums[track.albumID].cover,votes:prevTrack.votes});
+                            //On leur applique un id-2 car les 2 premières track sont shift
+                            proposedTracks.push({id:i-2,trackID:prevTrack.trackID,deezerID:track.deezerID,title:track.title,artist:albums[track.albumID].artist,cover:albums[track.albumID].cover,votes:prevTrack.votes});
                         }
                         currentTrack=proposedTracks.shift();
                         afterTrack=proposedTracks.shift();
@@ -72,6 +74,10 @@ var notInProposedTracks = function(data,id,callback){
 
 app.get('/playlist/currentTrack', function (request, response) {
     response.json(currentTrack);
+});
+
+app.get('/playlist/position', function (request, response) {
+    response.json(position);
 });
 
 app.get('/playlist/afterTrack', function (request, response) {
@@ -170,9 +176,9 @@ app.put('/playlist/:trackID', function(request, response) {
             notInProposedTracks(jsonData.data, trackID, function (id) {
                 console.log(id);
                 if (id > -1) {
-                    response.send(jsonData.data[id].vote.toString());
+                    response.send(jsonData.data[id].votes.toString());
                 } else {
-                    jsonData.data.push({trackID: trackID, votes: 0, status: 'playlist'});
+                    jsonData.data.push({id: jsonData.data.length, trackID: trackID, votes: 0, status: 'playlist'});
                     db.insert(jsonData, function (err, body) {
                         if (!err) {
                             response.send(body.ok);
@@ -197,11 +203,76 @@ app.post('/playlist/nextTrack', function (request, response) {
 
     response.json(currentTrack);
 });
+
+app.post('/playlist/calculVotes', function (request, response) {
+    var track = request.body;
+    if(currentTrack.trackID == track.trackID) {
+
+        var res = null;
+        var max = -1;
+        var index = -1;
+        for (var i = 0; i < proposedTracks.length; i++) {
+            if (proposedTracks[i].votes > max) {
+                max = proposedTracks[i].votes;
+                res = proposedTracks[i];
+                index = i;
+            }
+            //proposedTracks[i].votes = 0;
+        }
+        previousTracks.push(currentTrack);
+        if (previousTracks.length > 3)
+            previousTracks.shift();
+        if(afterTrack!=null)
+            currentTrack = afterTrack;
+        else{
+            var id = Math.floor((Math.random() * tracks.length));
+            currentTrack = {trackID:id,deezerID:tracks[id].deezerID,title:tracks[id].title,artist:albums[tracks[id].albumID].artist,cover:albums[tracks[id].albumID].cover};
+        }
+        position = 0;
+        afterTrack = res;
+        proposedTracks.splice(index, 1);
+
+        for (var i = 0; i < proposedTracks.length; i++) {
+            proposedTracks[i].id = i;
+        }
+        response.send("true");
+    }
+    else {
+        response.send("false");
+    }
+});
+
+app.post('/playlist/vote/:idTrack', function (request, response) {
+    var idTrack = parseInt(request.params.idTrack);
+    proposedTracks[idTrack].votes++;
+    response.json(proposedTracks[idTrack].votes);
+});
+
+app.post('/playlist/position' , function (request, response) {
+    var data = request.body;
+    if(data.pos > position && data.trackID == currentTrack.trackID)
+        position = data.pos;
+    //console.log(position);
+    response.send("ok");
+});
+app.post('/playlist/add/:trackID', function(request, response) {
+    var trackID = parseInt(request.params.trackID);
+    var track = tracks[trackID];
+    if(afterTrack==null){
+        afterTrack={id:proposedTracks.length,trackID:track.id,deezerID:track.deezerID,title:track.title,artist:albums[track.albumID].artist,cover:albums[track.albumID].cover};
+    }
+    notInProposedTracks(proposedTracks, trackID, function (id) {
+        if (id > -1 || afterTrack.deezerID==track.deezerID) {
+            response.send("already in playlist");
+        } else {
+            proposedTracks.push({id:proposedTracks.length,trackID:track.id,deezerID:track.deezerID,title:track.title,artist:albums[track.albumID].artist,cover:albums[track.albumID].cover,votes:0});
+            response.json(proposedTracks[proposedTracks.length-1]);
+        }
+    });
+
+
+});
+
 var server = app.listen(3000, function () {
     console.log('Listening at http://localhost:%s', server.address().port);
 });
-
-
-
-
-
