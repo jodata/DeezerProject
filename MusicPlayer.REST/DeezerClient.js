bda5cb3074695a99bd30c3c99f868ce3773f6b0e
Rémy <drouet.remy@gmail.com>
2015-04-22 15:02:40 +0200

    rectification bug changement icone du lecteur limitation vote feedback vote recherche dan sla playlist recherche sur Deezer

--- MusicPlayer.REST/DeezerClient.js
+++ MusicPlayer.REST/DeezerClient.js
@@ -6,26 +6,34 @@ var Deezer = module.exports = function() {
     var DEEZER_ARTIST_SEARCH = 'http://api.deezer.com/search/artist?q={_}';
     var DEEZER_ARTIST_ALBUMS = 'http://api.deezer.com/artist/{_}/albums';
     var DEEZER_SONGS_ALBUMS = 'http://api.deezer.com/album/{_}/tracks';
+    var DEEZER_QUERY        = 'http://api.deezer.com/search?q={_}';
 
     this.searchArtist = function (artistName, callback) {
         var url = DEEZER_ARTIST_SEARCH.replace('{_}', encodeURIComponent(artistName) );
-        new httpClient().GET(url, function (resp) {
+        new httpClient().GET(url, function (resp, err) {
             callback(JSON.parse(resp.body).data);
         });
     }; // method
 
     this.getArtistAlbums = function (artistID, callback) {
         var url = DEEZER_ARTIST_ALBUMS.replace('{_}', artistID);
-        new httpClient().GET(url, function (resp) {
-            callback(JSON.parse(resp.body).data);
+        new httpClient().GET(url, function (resp,err) {
+            callback(JSON.parse(resp.body).data,err);
         });
     }; // method
 
     this.getAlbumTracks = function (albumID, callback) {
         var url = DEEZER_SONGS_ALBUMS.replace('{_}', albumID);
-        new httpClient().GET(url, function (resp) {
-            callback(JSON.parse(resp.body).data);
+        new httpClient().GET(url, function (resp,err) {
+            callback(JSON.parse(resp.body).data,err);
         });
     }; // method
+
+    this.getSearchResult = function (query, callback) {
+        var url = DEEZER_QUERY.replace('{_}', query);
+        new httpClient().GET(url, function (resp,err) {
+            callback(JSON.parse(resp.body).data,err);
+        });
+    };// method
 
 } // class
